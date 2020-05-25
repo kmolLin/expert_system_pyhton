f6db1825b35892a6d449c29627d483496119da17
kmolLin <pyquino@gmail.com>
2020-05-25 11:54:30 +0800

    test1

--- animal.py
+++ animal.py
@@ -1,22 +1,27 @@
 # -*- coding: utf-8 -*-
- 
+
 # Form implementation generated from reading ui file 'animal.py'
 #
 # Created by: PyQt5 UI code generator 5.9.2
 #
 # WARNING! All changes made in this file will be lost!
 from ui_animal import Ui_Animals
-from PyQt5 import QtWidgets
-from PyQt5 import  QtGui
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtWidgets import QMessageBox
 import sys
 import os
 import tkinter
 from tkinter import messagebox
+
+
 def IsEvidence(x):
     for i in mywindow.fact:
         if x == i[-2]:
             return False
     return True
+
+
 def getData(x):
     data = []
     for i in mywindow.fact:
@@ -26,30 +31,32 @@ def getData(x):
                 tr.append(i[j])
             data.append(tr)
     return data
+
+
 def backs(data):
     c = 0
     flag = False
     for i in data:
         d = "if "
         for s in range(0, len(i)):
-            if s == len(i)-2:
+            if s == len(i) - 2:
                 d = d + str(i[s]) + " then "
             else:
                 d = d + str(i[s]) + " "
         window.TL.append(d)
         for j in range(0, len(i) - 1):
-            if (IsEvidence(i[j])):
+            if IsEvidence(i[j]):
                 root = tkinter.Tk()
                 root.withdraw()
-                a= messagebox.askquestion("提示", i[j]+"嗎")
-                #print(i[j] + "吗?")
-                #r = input()
+                a = messagebox.askquestion("提示", i[j] + "嗎")
+                print(i[j] + "吗?")
+                # r = input()
                 print(a)
-                if a == "yes":
+                if a == QMessageBox.Yes:
                     c = c + 1
             else:
                 temp = getData(i[j])
-                if (backs(temp)):
+                if backs(temp):
                     c = c + 1
         if c >= i.__len__() - 1:
             flag = True
@@ -60,14 +67,17 @@ def backs(data):
             flag = False
             print(i[-1])
             print("驗證失敗")
-    if (flag):
+    if flag:
         return True
     else:
         return False
-class  mywindow(QtWidgets.QWidget,Ui_Animals):
+
+
+class mywindow(QWidget, Ui_Animals):
     fact = []
     conditions = set("")
     res = set("")
+
     def __init__(self):
         super(mywindow, self).__init__()
         f = open("rules.txt", "r", encoding="utf-8")
@@ -76,36 +86,38 @@ class  mywindow(QtWidgets.QWidget,Ui_Animals):
             mywindow.fact.append(ls)
         f.close()
         for i in mywindow.fact:
-            for j in range(0,len(i)-2):
+            for j in range(0, len(i) - 2):
                 mywindow.conditions.add(i[j])
             mywindow.res.add(i[-2])
         self.setupUi(self)
+
     def resizeEvent(self, event):
-        palette = QtGui.QPalette()
+        palette = QPalette()
         # pix = QtGui.QPixmap('images/3.jpg')
         # pix = pix.scaled(self.width(), self.height())
         # palette.setBrush(QtGui.QPalette.Background, QtGui.QBrush(pix))
         # self.setPalette(palette)
+
     def test(self):
-        if self.checkBox.isChecked():#逆向推理
+        if self.checkBox.isChecked():  # 逆向推理
             i = self.comboBox.currentIndex()
             s = self.comboBox.itemText(i)
             print(s)
-            data=getData(s)
+            data = getData(s)
             print(data)
-            if (backs(data)):
+            if backs(data):
                 root = tkinter.Tk()
                 root.withdraw()
                 a = messagebox.showinfo("提示", "該動物是" + data[0][-1])
-                self.result.setText("專家系統分析該動物是"+data[0][-1])
+                self.result.setText("專家系統分析該動物是" + data[0][-1])
             else:
                 root = tkinter.Tk()
                 root.withdraw()
                 self.result.setText("專家分析該動物不是" + data[0][-1])
                 a = messagebox.showinfo("提示", "該動物不是" + data[0][-1])
-        else: #正向推理
-            s= self.input.toPlainText()
-            tl =""
+        else:  # 正向推理
+            s = self.input.toPlainText()
+            tl = ""
             description = s.split("\n")
             print("des")
             print(description)
@@ -121,20 +133,20 @@ class  mywindow(QtWidgets.QWidget,Ui_Animals):
                             break
                     if k == len(description):
                         break
- 
+
                 if same == i.__len__() - 2:
                     print("same=i")
                     line = 1
                     if i[-1] == "*":  # 是结论
                         d = "if "
-                        for s in range(0,len(i)-1) :
-                            if s == len(i)-3:
-                                d=d+str(i[s])+" then "
+                        for s in range(0, len(i) - 1):
+                            if s == len(i) - 3:
+                                d = d + str(i[s]) + " then "
                             else:
-                                d=d+str(i[s])+" "
+                                d = d + str(i[s]) + " "
                         tl = tl + d + "\n"
                         self.TL.setText(tl)
-                        self.result.setText("專家系統分析該動物是"+i[-2])
+                        self.result.setText("專家系統分析該動物是" + i[-2])
                         print(i[-2])
                     else:
                         line = 1
@@ -144,13 +156,13 @@ class  mywindow(QtWidgets.QWidget,Ui_Animals):
                                 d = d + str(i[s]) + " then "
                             else:
                                 d = d + str(i[s]) + " "
-                        tl = tl + d +"\n"
+                        tl = tl + d + "\n"
                         self.TL.setText(tl)
-                        self.result.setText("專家也不知道具體是什麽動物，大概率推測是"+i[-2])
-                        # print(i[-1])
+                        self.result.setText("專家也不知道具體是什麽動物，大概率推測是" + i[-2])
                         description.append(i[-2])
-            if line ==0:
+            if line == 0:
                 self.result.setText("專家也不知道具體是什麽動物")
+
     def selectInit(self):
         mywindow.fact.clear()
         mywindow.conditions.clear()
@@ -168,12 +180,13 @@ class  mywindow(QtWidgets.QWidget,Ui_Animals):
         self.input.clear()
         self.result.clear()
         self.TL.clear()
-        if(self.checkBox.isChecked()):
+        if self.checkBox.isChecked():
             for x in mywindow.res:
                 self.comboBox.addItem(str(x))
         else:
             for x in mywindow.conditions:
                 self.comboBox.addItem(str(x))
+
     def selectChange(self):
         if self.checkBox.isChecked():
             self.input.clear()
@@ -184,6 +197,7 @@ class  mywindow(QtWidgets.QWidget,Ui_Animals):
             i = self.comboBox.currentIndex()
             s = self.comboBox.itemText(i)
             self.input.append(s)
+
     def checkChange(self):
         self.comboBox.clear()
         if self.checkBox.isChecked():
@@ -192,9 +206,13 @@ class  mywindow(QtWidgets.QWidget,Ui_Animals):
         else:
             for x in mywindow.conditions:
                 self.comboBox.addItem(str(x))
+
     def rules(self):
         os.startfile('rules.txt')
-app = QtWidgets.QApplication(sys.argv)
-window = mywindow()
-window.show()
-sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = mywindow()
+    window.show()
+    sys.exit(app.exec_())
